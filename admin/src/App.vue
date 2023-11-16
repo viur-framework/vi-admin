8654ac8cb07cd6923471e7dae5d954506975fd4c
Andreas H. Kelch <>
2023-11-16 12:53:14 +0100

    chore: increases Version

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     const appStore = useAppStore()
 
     onMounted(() => {
-      appStore.state["vi.version"] = [4, 0, 13]
+      appStore.state["vi.version"] = [4, 0, 14]
     })
 
     function getPrimaryColor(lightness) {
