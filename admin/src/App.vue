02cd61d76ad5c1fed2e7c16cf43c577fb55bd674
Andreas H. Kelch <ak@mausbrand.de>
2023-11-21 13:37:53 +0100

    chore: increase Version

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     const appStore = useAppStore()
 
     onMounted(() => {
-      appStore.state["vi.version"] = [4, 0, 14]
+      appStore.state["vi.version"] = [4, 0, 15]
     })
 
     function getPrimaryColor(lightness) {
