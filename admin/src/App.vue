5b1f3565a34b970836032ed5973c0549b864a293
Andreas H. Kelch <>
2024-01-12 20:15:17 +0100

    chore: updated utils

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     const appStore = useAppStore()
 
     onMounted(() => {
-      appStore.state["vi.version"] = [4, 0, 22]
+      appStore.state["vi.version"] = [4, 0, 23]
     })
 
     function getPrimaryColor(lightness) {
