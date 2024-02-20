0d1b3aa610fff1cd8cacef3d6054fac639ca4074
Andreas H. Kelch <>
2024-02-20 14:55:04 +0100

    chore: update version

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -25,7 +25,7 @@ export default defineComponent({
     const appStore = useAppStore()
 
     onMounted(() => {
-      appStore.state["vi.version"] = [4, 1, 2]
+      appStore.state["vi.version"] = [4, 1, 3]
     })
 
     function getPrimaryColor(lightness) {
