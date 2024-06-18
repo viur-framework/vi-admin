9c62e290245029148d0677e3473dd63207f144ca
KadirBalku <Kadir.Balku@gmail.com>
2024-06-18 14:18:22 +0200

    chore: bump version

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -30,7 +30,7 @@ export default defineComponent({
     const appStore = useAppStore()
 
     onMounted(() => {
-      appStore.state["vi.version"] = [4, 2, 8]
+      appStore.state["vi.version"] = [4, 3, 1]
     })
 
     function getPrimaryColor(lightness) {
