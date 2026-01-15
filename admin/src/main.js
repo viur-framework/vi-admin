4a7cea4c7e0ebadabd17ceaf770ee548ee02d8ad
Andreas H. Kelch <32266000+akelch@users.noreply.github.com>
2026-01-15 13:25:34 +0100

    refactor: streamline i18n setup and update submodules
    Simplify the i18n integration by importing i18n from
    @viur/vue-components instead of manually defining locales
    and messages. This change reduces complexity and potential
    errors in translation setup.
    
    Update submodules to incorporate the latest changes
    from @viur/vue-components and @viur/vue-utils, ensuring
    compatibility and incorporating new features or bug fixes
    from these dependencies.

--- admin/src/main.js
+++ admin/src/main.js
@@ -13,9 +13,6 @@ initApp(app)
 
 import router from "./routes"
 import { createPinia } from "pinia"
-import { createI18n } from "vue-i18n"
-import en from "./translations/en"
-import de from "./translations/de"
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate"
 
 const pinia = createPinia()
@@ -31,16 +28,7 @@ app.component("WrapperNested", Wrapper_nested)
 
 app.component("ActionHandler", ActionHandler)
 
-// @ts-ignore
-import { de_translations, en_translations } from "@viur/vue-components/translations/translations"
-
-const i18n = createI18n({
-  legacy: false,
-  locale: "de",
-  fallbackLocale: "en",
-  messages: { en: { ...en_translations, ...en }, de: { ...de_translations, ...de } },
-})
-
+import { i18n } from "@viur/vue-components/i18n"
 app.use(i18n)
 
 app.mount("#app")
